c74c90cc83f769a117089c32c572bed1c039ffec
Stefurak Vasyl <vasilstifurak303@gmail.com>
2024-01-02 20:25:25 +0200

    create models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+from models.database import create_db
+from models.group import Group
+from models.students import Student
+from models.subject import Subject
+from models.user import User
+
+create_db()
