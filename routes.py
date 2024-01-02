a6c897ad5d8806c908dba0ca55ae388baeb83bcf
Stefurak Vasyl <vasilstifurak303@gmail.com>
2024-01-02 21:46:52 +0200

    create form

--- routes.py
+++ routes.py
@@ -6,18 +6,20 @@ from .models.user import User
 from flask import request, render_template, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
+from .forms import LoginForm, Studentform
+from .helpers import get_group_id, return_all_group_id
 
 
 @app.route("/main")
 @app.route("/group_management")
-def group_management():
+def group_management1():
     all_groups = session.query(Group).all()
     all_groups = [x.name_of_group for x in all_groups]
     return render_template("group.html", groups=all_groups)
 
 
 @app.route("/main")
-@app.route("/group_management", methods=["POST"])
+@app.route("/group__management", methods=["POST"])
 @login_required
 def group_management():
     name_of_group = request.form.get("name_of_group")
@@ -41,30 +43,40 @@ def group_list(group_name):
     return render_template("student.html", group=group)
 
 
-@app.route("/student_management/<group_name>", methods=["POST"])
-@login_required
-def group_list(group_name):
-    group_id = session.query(Group).where(Group.name_of_group == group_name).first().id
-    name = request.form["name"]
-    surname = request.form["surname"]
-    age = request.form["age"]
-    home_address = request.form["home_address"]
 
-    student = Student(
-        name=name,
-        surname=surname,
-        age=int(age),
-        home_address=home_address,
-        gr_id=group_id
-    )
-    try:
-        session.add(student)
-        session.commit()
-    except Exception as exc:
-        return f"При збереженні виникла проблема - {exc}"
-    finally:
-        session.close()
-        return redirect(f"/student_management/{group_name}")
+
+
+@app.route("/student_management/<group_name>", methods=["GET", "POST"])
+@login_required
+def group_list_(group_name):
+    form = Studentform()
+    group_id = get_group_id(group_name=group_name)
+    group = return_all_group_id(group_id)
+
+    if request.method == "POST":
+        group_id = session.query(Group).where(Group.name_of_group == group_name).first().id
+        name = request.form["name"]
+        surname = request.form["surname"]
+        age = request.form["age"]
+        home_address = request.form["home_address"]
+
+        student = Student(
+            name=name,
+            surname=surname,
+            age=int(age),
+            home_address=home_address,
+            gr_id=group_id
+        )
+        try:
+            session.add(student)
+            session.commit()
+        except Exception as exc:
+            return f"При збереженні виникла проблема - {exc}"
+        finally:
+            session.close()
+            return redirect(f"/student_management/{group_name}")
+    else:
+        return render_template("student.html", group=group, form=form)
 
 
 @app.route("/signup", methods=["POST", "GET"])
@@ -97,18 +109,8 @@ def signup():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    if request.method == "POST":
-        username = request.form["name"]
-        password = request.form["password"]
-        remember = True if request.form["remember"] else False
-
-        user = session.query(User).where(User.username == username).first()
-        if not user or not check_password_hash(user.password, password):
-            flash("Будь ласка, перевірте свій логін та пароль і спробуйте знову!")
-            return redirect("/login")
-        login_user(user=user, remember=remember)
-        return redirect(url_for("main"))
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", form=form)
 
 
 @app.route("/logout")
